da24e3defd1fb4a5ee6975e91cc3f09c0b11b033
tocudev <jalenbluee@gmail.com>
2026-09-17 11:20:23 -0600

    Added my server

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,6 @@ export const botTargets: Record<string, { author: string; support_server: string
   "1491864770490532061": { author: "risky", support_server: "https://discord.com/servers/riskys-server-894705593087049729" },
   "1540722224288366682": { author: "risky", support_server: "https://discord.com/servers/riskys-server-894705593087049729" },
   "1477798978362933340": { author: "matt", support_server: "https://discord.gg/ZrFDT7quVq" },
-  "1549993724249899098": { author: "alfon", support_server: "" },
+  "1477798978362933340": { author: "tocu", support_server: "https://discord.gg/RUVsM2HqC" },
+  "1537646323606757486": { author: "alfon", support_server: "" },
 }
