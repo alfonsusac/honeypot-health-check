3e7bcc9990d22332a9e726ab512adcd790718080
Alfonsus Ardani <alfonsusac@gmail.com>
2026-09-17 23:30:25 +0700

    added more bot

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,8 @@
  */
 export const botTargets: string[] = [
   "1450060292716494940", // Honeypot (risky's)
+  "1491864770490532061", // Honeypot Lite (risky's)
+  "1540722224288366682", // Report (risky's)
+  "1477798978362933340", // Novus (matt's)
   "1549993724249899098" // Honeypot Backup (alfon's)
 ]
