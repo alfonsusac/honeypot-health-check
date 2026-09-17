d5324b7650290ae7b4c4f3dbe2bfd3648e4eaa73
Alfonsus Ardani <alfonsusac@gmail.com>
2026-09-17 17:08:33 +0700

    update config

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,6 @@
  * log channel configured in the ENV.
  */
 export const botTargets: string[] = [
-  "1450060292716494940",
-];
+  "1450060292716494940", // Honeypot (risky's)
+  "1549993724249899098" // Honeypot Backup (alfon's)
+]
