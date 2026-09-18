f66f8571bf5c88909c2be8c985aa57ec8a4c3bb2
Alfonsus Ardani <alfonsusac@gmail.com>
2026-09-18 10:40:59 +0700

    added giftedly's

--- src/config.ts
+++ src/config.ts
@@ -7,5 +7,6 @@ export const botTargets: Record<string, {
   "1491864770490532061": { author: "risky", support_server: "https://discord.com/servers/riskys-server-894705593087049729", ping: true },
   "1540722224288366682": { author: "risky", support_server: "https://discord.com/servers/riskys-server-894705593087049729", ping: true },
   "1477798978362933340": { author: "matt", support_server: "https://discord.gg/ZrFDT7quVq", ping: true },
+  "1436973163211657278": { author: "giftedly", support_server: "https://discord.gg/PczBt78", ping: true },
   "1549993724249899098": { author: "alfon", support_server: "", ping: true },
 }
