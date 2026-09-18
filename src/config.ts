248be306eafc79e8d862fb8b88f75eb433aaa725
Alfonsus Ardani <alfonsusac@gmail.com>
2026-09-18 10:42:08 +0700

    added tocu's

--- src/config.ts
+++ src/config.ts
@@ -8,5 +8,6 @@ export const botTargets: Record<string, {
   "1540722224288366682": { author: "risky", support_server: "https://discord.com/servers/riskys-server-894705593087049729", ping: true },
   "1477798978362933340": { author: "matt", support_server: "https://discord.gg/ZrFDT7quVq", ping: true },
   "1436973163211657278": { author: "giftedly", support_server: "https://discord.gg/PczBt78", ping: true },
+  "1477798978362933340": { author: "tocu", support_server: "https://discord.gg/RUVsM2HqC", ping: true },
   "1549993724249899098": { author: "alfon", support_server: "", ping: true },
 }
