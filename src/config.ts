a0abfc09e7dfb6419e7ae6940c5c7c807877a60c
Alfonsus Ardani <alfonsusac@gmail.com>
2026-09-21 23:35:53 +0700

    add new bot

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,8 @@ export const botTargets: Record<string, {
   "1540722224288366682": { author: "risky", support_server: "https://discord.com/servers/riskys-server-894705593087049729", should_ping: true },
   "1477798978362933340": { author: "matt", support_server: "https://discord.gg/ZrFDT7quVq", should_ping: true },
   "1436973163211657278": { author: "giftedly", support_server: "https://discord.gg/PczBt78", should_ping: true },
+  "780657028695326720": { author: "risky", support_server: "https://discord.com/servers/riskys-server-894705593087049729", should_ping: true },
+  "1112023245332414534": { author: "maukkis", support_server: "", should_ping: true },
   "1549993724249899098": { author: "alfon", support_server: "", should_ping: true },
 }
 export const watchdogConfig = {
